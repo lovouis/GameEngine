813e1efa5ddb2a35d6482d1ed846142e8b104e01
tukeping <tukeping@gmail.com>
2018-01-19 20:18:59 +0800

    init

---
+++ src/main/java/com/ggg/Main.java
@@ -0,0 +1,11 @@
+package com.ggg;
+
+/**
+ * @author tukeping
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ggg GameEngine");
+    }
+}
